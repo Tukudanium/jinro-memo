33988b71aa54ceade963045a85b4773c7c038d83
Tukudani <zin9349@outlook.com>
2023-02-08 03:53:08 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: process.env.GITHUB_PAGES
-		? '/jinro-memo/' // レポジトリ名を設定
-		: './',
+	base: '/jinro-memo/', // レポジトリ名を設定
 	build: {
 		outDir: 'docs',
 		assetsDir: './'
