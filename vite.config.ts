f1ea3b1abd5dd5e900818bc376c6a02d4aa2013c
Tukudani <zin9349@outlook.com>
2024-07-31 01:42:13 +0900

    fix:アプリロケーションを変更 * GithubPagesと同じ設定は不可能なようなのでパスは指定しないように

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/jinro-memo/', // レポジトリ名を設定
+	base: '/', // レポジトリ名を設定
 	build: {
 		outDir: 'docs',
 		assetsDir: './'
